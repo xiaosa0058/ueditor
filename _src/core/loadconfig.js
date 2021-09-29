c4d07ba99db056832d71c983cb27ece7e02122ff
luoyisi@cheyutech.com <luoyisi@cheyutech.com>
2021-09-29 15:57:36 +0800

    配置本地化，取消远程获取

--- _src/core/loadconfig.js
+++ _src/core/loadconfig.js
@@ -12,13 +12,70 @@
             )
           );
 
-        var configUrl = me.getActionUrl("config"),
-          isJsonp = utils.isCrossDomainUrl(configUrl);
+        // var configUrl = me.getActionUrl("config"),
+        //   isJsonp = utils.isCrossDomainUrl(configUrl);
 
-        /* 发出ajax请求 */
+        /** LyS - 是否发出过ajax请求 */
         me._serverConfigLoaded = false;
+        // 配置本地化
+        var configJSON = {
+          'imageActionName': 'uploadimage',
+          'imageFieldName': 'file',
+          'imageMaxSize': 2048000,
+          'imageAllowFiles': ['.png', '.jpg', '.jpeg', '.gif', '.bmp'],
+          'imageCompressEnable': true,
+          'imageCompressBorder': 1600,
+          'imageInsertAlign': 'none',
+          'imageUrlPrefix': '',
+          'imagePathFormat': '\/ueditor\/php\/upload\/image\/{yyyy}{mm}{dd}\/{time}{rand:6}',
+          'scrawlActionName': 'uploadscrawl',
+          'scrawlFieldName': 'file',
+          'scrawlPathFormat': '\/ueditor\/php\/upload\/image\/{yyyy}{mm}{dd}\/{time}{rand:6}',
+          'scrawlMaxSize': 2048000,
+          'scrawlUrlPrefix': '',
+          'scrawlInsertAlign': 'none',
+          'snapscreenActionName': 'uploadimage',
+          'snapscreenPathFormat': '\/ueditor\/php\/upload\/image\/{yyyy}{mm}{dd}\/{time}{rand:6}',
+          'snapscreenUrlPrefix': '',
+          'snapscreenInsertAlign': 'none',
+          'catcherLocalDomain': ['127.0.0.1', 'localhost', 'img.baidu.com'],
+          'catcherActionName': 'catchimage',
+          'catcherFieldName': 'source',
+          'catcherPathFormat': '\/ueditor\/php\/upload\/image\/{yyyy}{mm}{dd}\/{time}{rand:6}',
+          'catcherUrlPrefix': '',
+          'catcherMaxSize': 2048000,
+          'catcherAllowFiles': ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.svg'],
+          'videoActionName': 'uploadvideo',
+          'videoFieldName': 'file',
+          'videoPathFormat': '\/ueditor\/php\/upload\/video\/{yyyy}{mm}{dd}\/{time}{rand:6}',
+          'videoUrlPrefix': '',
+          'videoMaxSize': 102400000,
+          'videoAllowFiles': ['.flv', '.swf', '.mkv', '.avi', '.rm', '.rmvb', '.mpeg', '.mpg', '.ogg', '.ogv', '.mov', '.wmv', '.mp4', '.webm', '.mp3', '.wav', '.mid'],
+          'fileActionName': 'uploadfile',
+          'fileFieldName': 'file',
+          'filePathFormat': '\/ueditor\/php\/upload\/file\/{yyyy}{mm}{dd}\/{time}{rand:6}',
+          'fileUrlPrefix': '',
+          'fileMaxSize': 51200000,
+          'fileAllowFiles': ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.flv', '.swf', '.mkv', '.avi', '.rm', '.rmvb', '.mpeg', '.mpg', '.ogg', '.ogv', '.mov', '.wmv', '.mp4', '.webm', '.mp3', '.wav', '.mid', '.rar', '.zip', '.tar', '.gz', '.7z', '.bz2', '.cab', '.iso', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf', '.txt', '.md', '.xml'],
+          'imageManagerActionName': 'listimage',
+          'imageManagerListPath': '\/ueditor\/php\/upload\/image\/',
+          'imageManagerListSize': 20,
+          'imageManagerUrlPrefix': '',
+          'imageManagerInsertAlign': 'none',
+          'imageManagerAllowFiles': ['.png', '.jpg', '.jpeg', '.gif', '.bmp'],
+          'fileManagerActionName': 'listfile',
+          'fileManagerListPath': '\/ueditor\/php\/upload\/file\/',
+          'fileManagerUrlPrefix': '',
+          'fileManagerListSize': 20,
+          'fileManagerAllowFiles': ['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.flv', '.swf', '.mkv', '.avi', '.rm', '.rmvb', '.mpeg', '.mpg', '.ogg', '.ogv', '.mov', '.wmv', '.mp4', '.webm', '.mp3', '.wav', '.mid', '.rar', '.zip', '.tar', '.gz', '.7z', '.bz2', '.cab', '.iso', '.doc', '.docx', '.xls', '.xlsx', '.ppt', '.pptx', '.pdf', '.txt', '.md', '.xml']
+        };
+        console.log('ueditor本地配置 =>', configJSON);
+        utils.extend(me.options, configJSON);
+        me.fireEvent('serverConfigLoaded');
+        // 标记为已发送过，因为isServerConfigLoaded方法中会进行判断
+        me._serverConfigLoaded = true
 
-        configUrl &&
+        /*configUrl &&
           UE.ajax.request(configUrl, {
             method: "GET",
             dataType: isJsonp ? "jsonp" : "",
@@ -35,7 +92,7 @@
             onerror: function() {
               showErrorMsg(me.getLang("loadconfigHttpError"));
             }
-          });
+          });*/
       } catch (e) {
         showErrorMsg(me.getLang("loadconfigError"));
       }
